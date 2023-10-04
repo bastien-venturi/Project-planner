9417869c9e9725b7a3b60e0d34cebd10a4cc6403
Venturi <bastien.aristide.venturi@gmail.com>
2023-10-04 10:28:44 +0200

    date

--- script/date.js
+++ script/date.js
@@ -1,56 +1,39 @@
+import {inputDueDate,createNewTask} from './form-add-list';
+
+
+    const enterdate = new Date(inputDueDate.value);
+    const currenttime = Date.now();
+    const userEnteredTime = enterdate.getTime();
+    const timeDifference = currenttime - userEnteredTime;
+    const daypass = Math.floor(timeDifference / (24 * 60 * 60 * 1000));
+    
+    displayduedate.textContent = ` ${daypass} days.`;
+
+
+
+
+ // Function for the current date
+ 
+ function formatDate(date) {
+     const options = {
+         year: '2-digit',
+         month: '2-digit',
+         day: '2-digit',
+     };
+ 
+ }
+ 
+ const datecreation = new Date();
+ const formattedate = formatDate(datecreation);
+ 
+ displaycreadate.textContent = formattedate;
+ 
+ console.log(displaycreadate);
+
+export {displaycreadate,displayduedate};
+
+/*
 const inputDate = document.createElement('input');
 inputDate.type = "date";
 inputDate.placeholder = 'Enter date';
-
-const calculatebutton = document.createElement('button');
-calculatebutton.textContent = 'GO!';
-calculatebutton.addEventListener('click', () => {
-    const userEnteredDate = new Date(inputDate.value);
-    const currenttime = Date.now();
-    const userEnteredTime = userEnteredDate.getTime();
-    const timeDifferencePrompt = currenttime - userEnteredTime;
-    const promptass = Math.floor(timeDifferencePrompt / (24 * 60 * 60 * 1000));
-    
-    remaining-time.textContent = ` ${promptass} days.`;
-});
-
-
-
-/*
-    <label for="dateInput">Entrez une date :</label>
-    <input type="date" id="dateInput">
-    <button onclick="calculateRemainingDays()">Calculer</button>
-    <p id="result"></p>
-
-
-
-
-function calculateRemainingDays() {
-    const dateInput = document.getElementById('dateInput').value;
-    const currentDate = new Date();
-    const selectedDate = new Date(dateInput);
-    const timeDifference = selectedDate - currentDate;
-    const remainingDays = Math.ceil(timeDifference / (1000 * 60 * 60 * 24));
-
-    if (remainingDays > 0) {
-        document.getElementById('result').textContent = `Il reste ${remainingDays} jours jusqu'à la date sélectionnée.`;
-    } else if (remainingDays === 0) {
-        document.getElementById('result').textContent = `Aujourd'hui est la date sélectionnée.`;
-    } else {
-        document.getElementById('result').textContent = `La date sélectionnée est passée.`;
-    }
-}
-
-
-
-
-
-  // Function for the current date
-  function date() {
-    const currentDate = new Date();
-    const year = currentDate.getFullYear();
-    const month = currentDate.getMonth() + 1;
-    const day = currentDate.getDate();
-    return `${day}/${month}/${year}`;
-}
 */
