f925c97e5b5a5e5096fa4e065c4687e4f9a2474f
Venturi <bastien.aristide.venturi@gmail.com>
2023-10-05 17:27:34 +0200

    fix bug merge

--- script/form-add-list.js
+++ script/form-add-list.js
@@ -61,7 +61,6 @@ let buttonGroup = document.createElement("div");
 // ---------------------------------------------
 let buttonAddList = createButton("Ajouter la liste", createNewList);
 let buttonCancel = createButton("Annuler", cancelListCreation);
-buttonCancel.style.marginLeft = "8px";
 buttonGroup.appendChild(buttonAddList);
 buttonGroup.appendChild(buttonCancel);
 
